628b609317a1ea60d3166898d7c7fc751f83305c
chenruimin <ruiminch@163.com>
2019-09-08 16:52:37 +0800

    added order data transformer to combine the same products in order item list

--- src/Modules/Order.php
+++ src/Modules/Order.php
@@ -9,6 +9,33 @@ namespace DataCue\Modules;
 class Order extends Base
 {
     /**
+     * Transform order data
+     * 
+     * @param $orderData
+     * @return array
+     */
+    public static function transformOrderData($orderData)
+    {
+        $cart = [];
+        foreach($orderData['cart'] as $orderItem) {
+            $existing = false;
+            foreach ($cart as &$item) {
+                if ($item['product_id'] === $orderItem['product_id'] && $item['variant_id'] === $orderItem['variant_id'] && $item['unit_price'] === $orderItem['unit_price']) {
+                    $item['quantity'] = intval($item['quantity']) + intval($orderItem['quantity']);
+                    $existing = true;
+                }
+            }
+            if (!$existing) {
+                $cart[] = $orderItem;
+            }
+        }
+
+        $orderData['cart'] = $cart;
+
+        return $orderData;
+    }
+
+    /**
      * Create order
      *
      * @param $orderData
@@ -23,7 +50,7 @@ class Order extends Base
      */
     public function create($orderData)
     {
-        return $this->request->post($this->url('orders'), $orderData);
+        return $this->request->post($this->url('orders'), static::transformOrderData($orderData));
     }
 
     /**
@@ -43,7 +70,9 @@ class Order extends Base
     {
         return $this->request->post($this->url('batch'), [
             'type' => 'orders',
-            'batch' => $orderDataList,
+            'batch' => array_map(function ($item) {
+                return static::transformOrderData($item);
+            }, $orderDataList),
         ]);
     }
 
